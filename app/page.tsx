6395f72dfec3c73dece74a820a3f6fa96e636e64
Sergio Scardigno <sergioscardigno82@gmail.com>
2024-10-12 10:02:40 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,136 +1,161 @@
-import Image from "next/image";
-import { get } from "@vercel/edge-config";
-import { redirect } from "next/navigation";
+import Image from 'next/image';
 
-function TwitterIcon() {
-  return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <title>Twitter</title>
-      <path
-        d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
-        fill="currentColor"
-      />
-    </svg>
-  );
+function FacebookIcon() {
+    return (
+        <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <title>Facebook</title>
+            <path
+                d="M22.675 0h-21.35C.6 0 0 .6 0 1.325v21.35C0 23.4.6 24 1.325 24h21.35C23.4 24 24 23.4 24 22.675V1.325C24 .6 23.4 0 22.675 0zm-2.225 12.219h-3.042v9.263h-4.11v-9.263h-2.848v-3.428h2.848v-2.517c0-2.805 1.24-4.56 4.658-4.56 1.25 0 2.56.225 2.56.225v3.196h-1.444c-1.39 0-1.833.867-1.833 1.753v2.015h3.42l-.54 3.428z"
+                fill="currentColor"
+            />
+        </svg>
+    );
 }
 
-function GithubIcon() {
-  return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <title>GitHub</title>
-      <path
-        d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
-        fill="currentColor"
-      />
-    </svg>
-  );
+function InstagramIcon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="24"
+            height="24"
+            viewBox="0 0 256 256"
+        >
+            <g
+                fill="#ffffff"
+                fillRule="nonzero" // Usar camelCase
+                stroke="none"
+                strokeWidth="1" // Usar camelCase
+                strokeLinecap="butt" // Usar camelCase
+                strokeLinejoin="miter" // Usar camelCase
+                strokeMiterlimit="10" // Usar camelCase
+                fontFamily="none"
+                fontWeight="none"
+                fontSize="none"
+                textAnchor="none"
+            >
+                <g transform="scale(8.53333,8.53333)">
+                    <path d="M9.99805,3c-3.859,0 -6.99805,3.14195 -6.99805,7.00195v10c0,3.859 3.14195,6.99805 7.00195,6.99805h10c3.859,0 6.99805,-3.14195 6.99805,-7.00195v-10c0,-3.859 -3.14195,-6.99805 -7.00195,-6.99805zM22,7c0.552,0 1,0.448 1,1c0,0.552 -0.448,1 -1,1c-0.552,0 -1,-0.448 -1,-1c0,-0.552 0.448,-1 1,-1zM15,9c3.309,0 6,2.691 6,6c0,3.309 -2.691,6 -6,6c-3.309,0 -6,-2.691 -6,-6c0,-3.309 2.691,-6 6,-6zM15,11c-2.20914,0 -4,1.79086 -4,4c0,2.20914 1.79086,4 4,4c2.20914,0 4,-1.79086 4,-4c0,-2.20914 -1.79086,-4 -4,-4z"></path>
+                </g>
+            </g>
+        </svg>
+    );
 }
+
+type Data = {
+    name: string;
+    avatar: string;
+    links: Array<{
+        href: string;
+        title: string;
+        image?: string;
+    }>;
+    socials: Array<{
+        href: string;
+        title: string;
+    }>;
+};
 
 function LinkCard({
-  href,
-  title,
-  image,
+    href,
+    title,
+    image,
 }: {
-  href: string;
-  title: string;
-  image?: string;
+    href: string;
+    title: string;
+    image?: string;
 }) {
-  return (
-    <a
-      href={href}
-      className="flex items-center p-1 w-full rounded-md hover:scale-105 transition-all 
+    return (
+        <a
+            href={href}
+            className="flex items-center p-1 w-full rounded-md hover:scale-105 transition-all 
       border border-gray-300 mb-3 bg-gray-100 max-w-3xl"
-    >
-      <div className="flex text-center w-full">
-        <div className="w-10 h-10">
-          {image && (
-            <Image
-              className="rounded-sm"
-              alt={title}
-              src={image}
-              width={40}
-              height={40}
-            />
-          )}
-        </div>
-        <h2
-          className="flex justify-center items-center 
-        font-semibold w-full text-gray-700 -ml-10"
         >
-          {title}
-        </h2>
-      </div>
-    </a>
-  );
+            <div className="flex text-center w-full">
+                {image && ( // Mostrar solo si existe la imagen
+                    <div className="w-10 h-10">
+                        <Image
+                            className="rounded-sm"
+                            alt={title}
+                            src={image}
+                            width={40}
+                            height={40}
+                        />
+                    </div>
+                )}
+                <h2
+                    className="flex justify-center items-center 
+        font-semibold w-full text-gray-700 -ml-10"
+                >
+                    {title}
+                </h2>
+            </div>
+        </a>
+    );
 }
 
-interface Data {
-  name: string;
-  avatar: string;
-  links: Link[];
-  socials: Social[];
-}
+const data: Data = {
+    name: 'TA Impresiones 3D',
+    avatar: '/img/logo.png',
+    links: [
+        {
+            href: 'https://www.instagram.com/taimpresiones3dlaplata/',
+            title: 'Instagram',
+            image: '/img/instagram.png',
+        },
+    ],
+    socials: [
+        {
+            href: 'https://web.facebook.com/ta.impresiones',
+            title: 'Facebook',
+        },
+        {
+            href: 'https://www.instagram.com/taimpresiones3dlaplata/',
+            title: 'Instagram',
+        },
+    ],
+};
 
-interface Link {
-  href: string;
-  title: string;
-  image?: string;
-}
-
-interface Social {
-  href: string;
-  title: string;
-  image?: string;
-}
-
-export default async function HomePage() {
-  const data = await get<Data>("linktree");
-
-  if (!data) {
-    redirect("https://linktr.ee/selenagomez");
-  }
-
-  return (
-    <div
-      className="flex items-center flex-col mx-auto w-full justify-center mt-16
+export default function HomePage() {
+    return (
+        <div
+            className="flex items-center flex-col mx-auto w-full justify-center mt-16
     px-8"
-    >
-      <Image
-        className="rounded-full"
-        alt={data.name}
-        src={data.avatar}
-        width={96}
-        height={96}
-      />
+        >
+            <Image
+                className="rounded-full"
+                alt={data.name}
+                src={data.avatar}
+                width={96}
+                height={96}
+            />
 
-      <h1 className="font-bold mt-4 text-xl mb-8 text-white">{data.name}</h1>
+            <h1 className="font-bold mt-4 text-xl mb-8 text-white">
+                {data.name}
+            </h1>
 
-      {data.links.map((link) => (
-        <LinkCard key={link.href} {...link} />
-      ))}
+            {data.links.map((link) => (
+                <LinkCard key={link.href} {...link} />
+            ))}
 
-      <div className="flex items-center gap-4 mt-8 text-white">
-        {data.socials.map((social) => {
-          if (social.href.includes("twitter")) {
-            return <TwitterIcon key={social.href} />;
-          }
-          if (social.href.includes("github")) {
-            return <GithubIcon key={social.href} />;
-          }
-        })}
-      </div>
-    </div>
-  );
+            <div className="flex items-center gap-4 mt-8 text-white">
+                {data.socials.map((social) => {
+                    if (social.href.includes('facebook')) {
+                        return <FacebookIcon key={social.href} />;
+                    }
+                    if (social.href.includes('instagram')) {
+                        return <InstagramIcon key={social.href} />;
+                    }
+                    return null;
+                })}
+            </div>
+        </div>
+    );
 }
