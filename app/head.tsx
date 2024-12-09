cc853fc94f227ad004e96333158538516215eef9
Sergio Scardigno <sergioscardigno82@gmail.com>
2024-12-09 09:22:25 -0300

    asd

--- app/head.tsx
+++ app/head.tsx
@@ -1,10 +1,16 @@
 export default function Head() {
-  return (
-    <>
-      <title>Linktree</title>
-      <meta name="description" content="Next.js + Tailwind CSS" />
-      <link rel="icon" href="/favicon.ico" />
-      <meta name="viewport" content="width=device-width,initial-scale=1" />
-    </>
-  );
+    return (
+        <>
+            <title>TA Impresiones 3D La Plata</title>
+            <meta
+                name="description"
+                content="TA Impresiones 3D La Plata - Impresiones 3D, Modelado 3D, Diseño 3D, Impresiones 3D en La Plata"
+            />
+            <link rel="icon" href="/favicon.ico" />
+            <meta
+                name="viewport"
+                content="width=device-width,initial-scale=1"
+            />
+        </>
+    );
 }
